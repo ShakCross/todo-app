8e1d3746ea40d583101d6cad38696a37ce905241
André Santa Cruz <shakcross@hotmail.com>
2023-01-05 02:29:49 -0500

    Updates

---
+++ precache-manifest.9e1f3f00b3dc7e177fa0c077ae74f6b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab51dca0906d6d83ef4c0e043535c72",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "2e93745f9bcb6843125f",
+    "url": "/Todo-App/static/css/main.1a9c960e.chunk.css"
+  },
+  {
+    "revision": "3fa91010280522e5939e",
+    "url": "/Todo-App/static/js/2.628b80f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App/static/js/2.628b80f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e93745f9bcb6843125f",
+    "url": "/Todo-App/static/js/main.c872da30.chunk.js"
+  },
+  {
+    "revision": "7ce302720659a9d71564",
+    "url": "/Todo-App/static/js/runtime-main.557973aa.js"
+  }
+]);
